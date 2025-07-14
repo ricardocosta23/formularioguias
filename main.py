3f57d31ce7fb80aec9c16b708e2399213f02923a
ricardocosta23 <ricardo_costa23@outlook.com>
2025-07-14 12:57:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def handler(request):
 
 # Also keep the original for local development
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
